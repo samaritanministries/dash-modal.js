d88c46f29c43267de7d2972b948301e4d2f6951b
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-03-02 09:13:55 -0600

    - Change example header text to h3 - Add negative margin-left to center header text - Reposition modal close button for h3 header text

--- scripts/sample_app/modal_view.js
+++ scripts/sample_app/modal_view.js
@@ -4,7 +4,7 @@ export default class extends Backbone.View {
 
   render() {
     this.$el.html(
-      '<div class="modal-header"><h2>Example Modal</h2></div>' +
+      '<div class="modal-header"><h3>Example Modal</h3></div>' +
       '<div class="modal-content"><p>Example content in a modal for all the people to see. It is pretty cool huh? I love content. Especially stuff that does not make sense.</p></div>' +
       '<div class="modal-footer">Footer content</div>'
     )
